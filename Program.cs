abb3469470b2166306020732b4581aed760f0f3e
Artem Sorokin <artem.sorokin@teb.edu.pl>
2025-03-12 11:55:52 +0100

    sprwadzenie trojkat

--- Program.cs
+++ Program.cs
@@ -1,25 +1,46 @@
 ﻿bool kont = true;
 
+static int zad3()
+{
+    int liczba;
+    while (true)
+    {
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out liczba))
+        {
+            return liczba;
+        }
+        else
+        {
+            Console.WriteLine("Wprowadź poprawną liczbę.");
+        }
+    }
+}
+
+
 while (kont)
 {
 
-Console.WriteLine("Podaj pierwszą liczbę:");
-int a = int.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine("Podaj pierwszą liczbę:");
+    int a = zad3();
 
-Console.WriteLine("Podaj drugą liczbę:");
-int b = int.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine("Podaj drugą liczbę:");
+    int b = zad3();
 
-Console.WriteLine("Podaj trzecią liczbę:");
-int c = int.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine("Podaj trzecią liczbę:");
+    int c = zad3();
 
-if (a * a + b * b == c * c)
-{
-    Console.WriteLine("To jest trójkąt pitagoreski");
-}
-else
-{
-    Console.WriteLine("To nie jest trójkąt pitagoreski");
-}
+    int[] num = { a, b, c };
+    Array.Sort(num);
+
+    if (num[0] * num[0] + num[1] * num[1] == num[2] * num[2])
+    {
+        Console.WriteLine("To jest trójkąt pitagoreski");
+    }
+    else
+    {
+        Console.WriteLine("To nie jest trójkąt pitagoreski");
+    }
 
     Console.WriteLine("Czy chcesz kontynuować? (tak/nie)");
     string? odp = Console.ReadLine()?.ToLower();
@@ -29,4 +50,5 @@ else
         kont = false;
     }
 
-}
+};
+
